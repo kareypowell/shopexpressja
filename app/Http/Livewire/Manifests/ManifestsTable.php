c5cee72a1ad26f3a0844c2f775b4979fb8a3fef1
Karey Powell <kareypowell@gmail.com>
2025-05-13 23:49:21 -0500

    re-enable live table refresh

--- app/Http/Livewire/Manifests/ManifestsTable.php
+++ app/Http/Livewire/Manifests/ManifestsTable.php
@@ -10,7 +10,7 @@ use App\Models\Manifest;
 
 class ManifestsTable extends DataTableComponent
 {
-    // public $refresh = 'visible';
+    public $refresh = 'visible';
 
     public array $filterNames = [
         'type' => 'Type',
