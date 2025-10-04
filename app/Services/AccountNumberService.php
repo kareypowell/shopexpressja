ce37fd1ca84b3f29ca7802787a0c6058d58f61cf
Karey Powell <kareypowell@gmail.com>
2025-10-04 10:12:10 -0500

    Reduce account number to 4-chars, 4-digits

--- app/Services/AccountNumberService.php
+++ app/Services/AccountNumberService.php
@@ -36,7 +36,7 @@ class AccountNumberService
      */
     private function generateAccountNumber(): string
     {
-        return 'ALQS' . mt_rand(1000000, 9999999);
+        return 'ALQS' . mt_rand(1000, 9999);
     }
 
     /**
